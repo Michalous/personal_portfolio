d9feb0dae027f49e5ccb946016a7e4f65b2197fd
Michalous <49964028+Michalous@users.noreply.github.com>
2023-01-21 11:52:31 +0000

    canvas clears on next round

--- portfolio/static/portfolio/assets/Hangman/logic.js
+++ portfolio/static/portfolio/assets/Hangman/logic.js
@@ -153,6 +153,7 @@ document.addEventListener('DOMContentLoaded', function() {
             $('#letters').hide()
             $('#display_score_win').text(score)
             $('#play_again_win').click(function() {
+                context.clearRect(0, 0, canvas.width, canvas.height)
                 word_to_guess = loadDoc()['word']
                 wrong_guesses = 0
                 remaining_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
